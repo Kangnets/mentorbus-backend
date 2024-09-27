d812b754deccc9d78c4895db18c3c58687f16377
Kangnets <kangnets88@gmail.com>
2024-09-27 20:52:23 +0900

    mentorbusDB로 데이터 베이스 교체2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 // Connection pool 생성
 const pool = mysql.createPool({
   host: "127.0.0.1",
+  port: 3306,
   user: "mentorowner",
   password: "kangwh05!!",
   database: "mentorbusDB",
