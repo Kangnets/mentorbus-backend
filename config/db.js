a2a80c1b6719b814bd6217cdf2b4c00555fc0fdc
Kangnets <kangnets88@gmail.com>
2024-09-27 21:44:13 +0900

    maria db 배포 후 연결 7

--- config/db.js
+++ config/db.js
@@ -1,5 +1,3 @@
-const mysql = require("mysql");
-
 const config = {
   host: "svc.sel4.cloudtype.app",
   port: 30957,
@@ -8,14 +6,4 @@ const config = {
   database: "mentorbus_db",
 };
 
-module.exports = {
-  init: function () {
-    return mysql.createConnection(config);
-  },
-  connect: function (conn) {
-    conn.connect(function (err) {
-      if (err) console.error("mysql 연결 에러 : " + err);
-      else console.log("mysql 연결 성공");
-    });
-  },
-};
+module.exports = { config };
