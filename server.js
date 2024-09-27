e331c834bfa5d5f6f773ae9b15fa005c95df3285
Kangnets <kangnets88@gmail.com>
2024-09-27 17:33:40 +0900

    connect ECONNREFUSED 에러 수정 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 // Connection pool 생성
 const pool = mysql.createPool({
-  host: "127.0.0.1",
+  host: "::1",
   user: "mentorowner",
   password: "kangwh05!!",
   database: "mentorbus_db",
