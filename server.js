af1011e3190c443211464dabd9f57e3f565c8009
Kangnets <kangnets88@gmail.com>
2024-09-30 09:39:06 +0900

    class 가져오기 수정 10

--- server.js
+++ server.js
@@ -400,7 +400,7 @@ app.post("/class/save", (req, res) => {
       }
       res
         .status(200)
-        .json({ message: "Class data saved successfully", results });
+        .json({ message: "Class data saved successfully", result: res });
     }
   );
 });
